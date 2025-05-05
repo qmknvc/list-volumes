bbaa59f2b1093cb3ab0ea3cdd493e970bdd18d7c
qmknvc <alem.mknvc@gmail.com>
2025-05-05 05:54:19 +0200

    Reverted some changes

--- database/db.js
+++ database/db.js
@@ -6,28 +6,12 @@ db.configure({
     password: 'alem',
     database: 'usb_app'
 });
-const saveToDB = async (user_email, user_password, user_phone) => {
+const saveToDB = async (user_email, user_phone) => {
     try {
         await db.query('INSERT INTO users (user_email, user_phone) VALUES (?, ?)', [user_email, user_phone]);
       } catch (error) {
         console.error('Error saving data:', error);
       } 
-}
-
-const validateEmail = async (user_email) => {
-  try {
-      const [rows] = await db.query(
-          'SELECT user_password FROM users WHERE user_email = ?',
-          [user_email]
-      );
-      if (rows.length === 0) {
-          return null;
-      }
-      return rows[0].user_password;
-  } catch (error) {
-      console.error('Error checking user credentials:', error);
-      return null;
-  }
 }
 
 export { saveToDB };
